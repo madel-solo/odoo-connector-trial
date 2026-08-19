8db309efe46aaf7d067dd3fdcde3945aec2e3414
Mohamed <m.adel@orchida-soft.com>
2026-08-19 13:35:32 +0300

    Reuse connector technical name

--- orchida_uae_e_invoicing/__manifest__.py
+++ orchida_uae_e_invoicing/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Orchida UAE E-Invoicing Connector for Odoo | FTA & PINT AE',
-    'version': '16.0.1.0.4',
+    'version': '16.0.1.0.5',
     'summary': 'UAE e-invoicing, UAE e invoicing, UAE e-invoice, UAE e invoice, UAE invoice, UAE invoicing, UAE einvoice, UAE einvoicing',
     'description': """
         Trial/demo module that reproduces the Orchida UAE E-Invoicing
